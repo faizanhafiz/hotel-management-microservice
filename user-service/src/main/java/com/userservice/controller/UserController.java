8cf63b8322d81afd2888c5410d62e624f37e49ad
=Faizan Ahmad <=faizanhafiz928@gmail.com>
2023-06-22 22:59:55 +0530

    "UserController is implemented"

--- user-service/src/main/java/com/userservice/controller/UserController.java
+++ user-service/src/main/java/com/userservice/controller/UserController.java
@@ -1,10 +1,53 @@
 package com.userservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.userservice.entities.User;
+import com.userservice.exceptions.UserNotFoundException;
+import com.userservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+    //create User Endpoint
+
+
+    @PostMapping("/addUser")
+    public ResponseEntity<User> createUser(@RequestBody  User user)
+    {
+
+        User userResponse  = userService.saveUser(user);
+
+        return   ResponseEntity.ok(userResponse);
+
+    }
+
+
+    // getAll User
+
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUser()
+    {
+        List<User>  allUsers = userService.getAllUser();
+
+        return ResponseEntity.ok(allUsers);
+    }
+
+    //getOneUser
+
+    @GetMapping("/getuser/{id}")
+    public ResponseEntity<User>  getUser(@PathVariable  int id) throws UserNotFoundException {
+        User user = userService.getUser(id);
+        return ResponseEntity.ok(user);
+    }
+
+
 }
--- user-service/src/main/java/com/userservice/entities/User.java
+++ user-service/src/main/java/com/userservice/entities/User.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
@@ -20,6 +17,7 @@ public class User {
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userId")
     private int userId ;
 
