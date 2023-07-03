e23d279f84b290c88e9f85692344fd08822e3a01
=Faizan Ahmad <=faizanhafiz928@gmail.com>
2023-07-03 17:53:11 +0530

    @EnableConfigServer

--- hotel/src/main/java/com/hotel/HotelApplication.java
+++ hotel/src/main/java/com/hotel/HotelApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class HotelApplication {
 
 	public static void main(String[] args) {
